488779f056efa1f6d07939276f31b4522e6d9939
AliefWahyulianto <aliefwahyulianto11@gmail.com>
2026-04-23 13:47:36 +0700

    hari ke-4: rebuild CRUD progress

---
+++ resources/views/produk/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Produk</title>
+</head>
+<body>
+    <h1>Data Produk</h1>
+    <a href="/produk/create">Tambah Produk</a>
+
+    <table border ="1">
+        <tr>
+            <th>ID</th>
+            <th>Nama Barang</th>
+            <th>Harga</th>
+            <th>Stok</th>
+            <th>Aksi</th>
+        </tr>
+        @foreach($produk as $item)
+        <tr>
+            <td>{{$item->id}}</td>
+            <td>{{$item->nama_barang}}</td>
+            <td>{{$item->harga}}</td>
+            <td>{{$item->stok}}</td>
+            <td>
+                <a href="/produk/{{$item->id}}/edit">Edit</a>
+                <form action="/produk{{$item->id}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Hapus</button>
+                </form>
+            </td>
+        </tr>]
+        @endforeach
+    </table>
+</body>
+</html>
